651af44d827749420dcca1943d082760f9d30419
mega-web-project <fobilouis17@gmail.com>
2021-11-18 04:56:04 -0800

    louis commit

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+       autoplay:true,
+       autohoverpause:true,
+       autoplaytimeout:100,
+       items:4,
+       nav:true,
+       loop:true,
+       lazyLoad:true,
+       margin:5,
+       padding:5,
+       stagePadding:5,
+       responsive:{
+        0:{
+            items:1,
+            dots:false,
+        },
+        485:{
+            items:2,
+            dots:false
+        },
+        728:{
+            items:2,
+            dots:false
+        },
+        960:{
+            items:2,
+            dots:false
+        },
+        1000:{
+            items:4,
+            dots:true
+        }
+
+    }
+    })
+})
--- src/php/index.php
+++ src/php/index.php
@@ -12,9 +12,13 @@
     <link rel="stylesheet" href="../../fonts/css/all.min.css">
     <link rel="stylesheet" href="../../fonts/css/fontawesome.min.css">
     <!--navigation styles--->
-    
      <link href="../css/navs.css" rel="stylesheet">
     <link href="../css/carouselSearch.css" rel="stylesheet">
+    <link href="../css/index.css" rel="stylesheet">
+
+    <!--css/owl-carousel-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>LearnaCart | Course page</title>
   </head>
   <body>
@@ -105,85 +109,209 @@
   </button>
 </div>
 <!--Caption carousel ends here-->
-    <!--1st row Cards placed here-->
-    <div class="row row-cols-1 row-cols-md-4 g-4 row-1">
-        <div class="col">
-          <div class="card">
-            <img src="../images/account.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="../images/account.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card card-3">
-            <img src="../images/arch.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-            <div class="card">
-              <img src="../images/curriculum.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
-            </div>
-          </div>
+
+
+<!--card carousel-->
+<div class="container">
+<h2 class="owl-card-title text-center fw-bold display-5">New <span class="text-danger">Course</span></h2>
+<div class="owl-carousel owl-theme">
+  <div class="ms-2 me-2">
+    <div class="card">
+     <img data-src="../images/account.jpg" alt="account" class="owl-lazy">
+     <div class="card-body">
+       <h5 clas="card-title">Course Title</h5>
+       <p>Your Description here</p>
+      </div>
+    </div>
+    </div>
+    
+    <div class="ms-2 me-2">
+    <div class="card">
+     <img data-src="../images/curriculum.jpg" alt="account" class="owl-lazy">
+     <div class="card-body">
+       <h5 clas="card-title">Course Title</h5>
+       <p>Your Description here</p>
+      </div>
+    </div>
     </div>
 
-    <!--Second row cards placed here-->
-    <div class="row row-cols-1 row-cols-md-4 g-4 row-1">
-        <div class="col">
-          <div class="card">
-            <img src="../images/account.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="../images/account.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card card-3">
-            <img src="../images/arch.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-            <div class="card">
-              <img src="../images/curriculum.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
-            </div>
-          </div>
+    <div class="ms-2 me-2">
+    <div class="card">
+     <img data-src="../images/java.jpg" alt="account" class="owl-lazy">
+     <div class="card-body">
+       <h5 clas="card-title">Course Title</h5>
+       <p>Your Description here</p>
+      </div>
     </div>
+    </div>
+  
+    <div class="ms-2 me-2">
+    <div class="card">
+     <img data-src="../images/data.jpg" alt="account" class="owl-lazy">
+     <div class="card-body">
+       <h5 clas="card-title">Course Title</h5>
+       <p>Your Description here</p>
+      </div>
+    </div>
+    </div>
+    <div class="ms-2 me-2">
+    <div class="card">
+     <img data-src="../images/arch.jpg" alt="account" class="owl-lazy">
+     <div class="card-body">
+       <h5 clas="card-title">Course Title</h5>
+       <p>Your Description here</p>
+      </div>
+    </div>
+    </div>
+</div>
+
+
+<div class="row courses">
+  <div class="col-md-3 col-sm-6 my-4">
+    <div class="card text-center">
+    <span class="position-absolute top-5 start-50 translate-middle badge rounded-pill bg-danger">
+      GH$5.00
+    <span class="visually-hidden">unread messages</span>
+  </span>
+      <img src="../images/data.jpg" alt="account" class="img-fluid">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>title 1</h4>
+        </div>
+        <div class="card-text">Write something here</div>
+      <a href="#" class="btn btn-success">View Course</a>
+      </div>
+    </div>
+  </div>
+
+<!--second card-->
+<div class="col-md-3 col-sm-6 my-4">
+    <div class="card text-center">
+    <span class="position-absolute top-5 start-50 translate-middle badge rounded-pill bg-danger">
+      GH$5.00
+    <span class="visually-hidden">unread messages</span>
+  </span>
+      <img src="../images/account.jpg" alt="account" class="img-fluid">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>title 1</h4>
+        </div>
+        <div class="card-text">Write something here</div>
+      <a href="#" class="btn btn-success">View Course</a>
+      </div>
+    </div>
+  </div>
+  <!--next card-->
+  <div class="col-md-3 col-sm-6 my-4">
+    <div class="card text-center">
+    <span class="position-absolute top-5 start-50 translate-middle badge rounded-pill bg-danger">
+      GH$5.00
+    <span class="visually-hidden">unread messages</span>
+  </span>
+      <img src="../images/java.jpg" alt="account" class="img-fluid">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>title 1</h4>
+        </div>
+        <div class="card-text">Write something here</div>
+      <a href="#" class="btn btn-success">View Course</a>
+      </div>
+    </div>
+  </div>
+  <!--next card-->
+  <div class="col-md-3 col-sm-6 my-4">
+    <div class="card text-center">
+    <span class="position-absolute top-5 start-50 translate-middle badge rounded-pill bg-danger">
+      GH$5.00
+    <span class="visually-hidden">unread messages</span>
+  </span>
+      <img src="../images/arch.jpg" alt="account" class="img-fluid">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>title 1</h4>
+        </div>
+        <div class="card-text">Write something here</div>
+      <a href="#" class="btn btn-success">View Course</a>
+      </div>
+    </div>
+  </div> 
+</div>
+
+<div class="row courses">
+  <div class="col-md-3 col-sm-6 my-4">
+    <div class="card text-center">
+    <span class="position-absolute top-5 start-50 translate-middle badge rounded-pill bg-danger">
+      GH$5.00
+    <span class="visually-hidden">unread messages</span>
+  </span>
+      <img src="../images/account.jpg" alt="account" class="img-fluid">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>title 1</h4>
+        </div>
+        <div class="card-text">Write something here</div>
+      <a href="#" class="btn btn-success">View Course</a>
+      </div>
+    </div>
+  </div>
+
+<!--second card-->
+<div class="col-md-3 col-sm-6 my-4">
+    <div class="card text-center">
+    <span class="position-absolute top-5 start-50 translate-middle badge rounded-pill bg-danger">
+      GH$5.00
+    <span class="visually-hidden">unread messages</span>
+  </span>
+      <img src="../images/arch.jpg" alt="account" class="img-fluid">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>title 1</h4>
+        </div>
+        <div class="card-text">Write something here</div>
+      <a href="#" class="btn btn-success">View Course</a>
+      </div>
+    </div>
+  </div>
+  <!--next card-->
+  <div class="col-md-3 col-sm-6 my-4">
+    <div class="card text-center">
+    <span class="position-absolute top-5 start-50 translate-middle badge rounded-pill bg-danger">
+      GH$5.00
+    <span class="visually-hidden">unread messages</span>
+  </span>
+      <img src="../images/java.jpg" alt="account" class="img-fluid">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>title 1</h4>
+        </div>
+        <div class="card-text">Write something here</div>
+      <a href="#" class="btn btn-success">View Course</a>
+      </div>
+    </div>
+  </div>
+  <!--next card-->
+  <div class="col-md-3 col-sm-6 my-4">
+    <div class="card text-center">
+    <span class="position-absolute top-5 start-50 translate-middle badge rounded-pill bg-danger">
+      GH$5.00
+    <span class="visually-hidden">unread messages</span>
+  </span>
+      <img src="../images/data.jpg" alt="account" class="img-fluid">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>title 1</h4>
+        </div>
+        <div class="card-text">Write something here</div>
+      <a href="#" class="btn btn-success">View Course</a>
+      </div>
+    </div>
+  </div> 
+</div>
+</div>
+
+<!--card carousel end here-->
+    <!--1st row Cards placed here-->
+   
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -194,5 +322,10 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/main.js"></script>
+  <!--owl carousel js cdn-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  </script>
   </body>
 </html>
